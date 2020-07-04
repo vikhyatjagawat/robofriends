f249d5836e93b306eb6cb929db3b0e82527081c7
ArsenalVault <vikhyatjagawat007@gmail.com>
2020-07-04 12:09:37 +0530

    Final Project

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import SearchBox from './SearchBox';
 
 const Card = (props) => {
     return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 //import Card from './Card';
 //import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
